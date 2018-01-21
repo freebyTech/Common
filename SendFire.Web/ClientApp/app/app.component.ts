f5bd217f22d34c008c5c27fe862aef40138d41b0
James Eby <temporafugiunt@gmail.com>
2018-01-21 00:41:27 -0600

    Repurposing another open source project I started for new product.

--- SendFire.Web/ClientApp/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app',
-    template: `<sf-topnav></sf-topnav>
-                <router-outlet></router-outlet>`
-})
-export class AppComponent {
-}
