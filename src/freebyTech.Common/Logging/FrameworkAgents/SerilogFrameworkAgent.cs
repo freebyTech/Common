9e8d22e3096b69af8becba713e79ea669a39e882
James Eby <admin@freebytech.com>
2022-07-08 16:02:09 -0500

    Adding exception context to Serilog.

--- src/freebyTech.Common/Logging/FrameworkAgents/SerilogFrameworkAgent.cs
+++ src/freebyTech.Common/Logging/FrameworkAgents/SerilogFrameworkAgent.cs
@@ -10,57 +10,64 @@ using freebyTech.Common.Logging.Interfaces;
 
 namespace freebyTech.Common.Logging.FrameworkAgents
 {
-    /// <summary>
-    /// This interface represents an actual logging agent that takes the logging abstraction and translates it to each framework.
-    /// </summary>
-    public class SerilogFrameworkAgent : ILogFrameworkAgent
+  /// <summary>
+  /// This interface represents an actual logging agent that takes the logging abstraction and translates it to each framework.
+  /// </summary>
+  public class SerilogFrameworkAgent : ILogFrameworkAgent
+  {
+    public void Log(string loggerName, GenericLogEventInfo logEvent)
     {
-        public void Log(string loggerName, GenericLogEventInfo logEvent) {
-            var logger = Serilog.Log.Logger.ForContext("loggerName", loggerName)
-            .ForContext("data", logEvent.Data)
-            .ForContext("internalLog", AggregatePushLogItems(logEvent.PushLog))
-            .ForContext("messageType", logEvent.MessageType);
+      var logger = Serilog.Log.Logger.ForContext("loggerName", loggerName)
+      .ForContext("data", logEvent.Data)
+      .ForContext("internalLog", AggregatePushLogItems(logEvent.PushLog))
+      .ForContext("messageType", logEvent.MessageType)
+      .ForContext("Exception", logEvent.Exception);
 
-            try{
-                foreach(var extraProperty in logEvent.ExtraProperties) {
-                    logger = logger.ForContext(extraProperty.Key, extraProperty.Value);
-                }
-            }
-            catch(Exception ex) {
-                Debug.Assert(true, "Serilog Agent Context Generation Threw Exception", ex.Message);
-            }
-            
-            logger.Write(GenericToSpecificLogLevel(logEvent.LogLevel), logEvent.Exception, logEvent.Message);
-        }
-
-        private static LogEventLevel GenericToSpecificLogLevel(GenericLogLevel logLevel) {
-            switch(logLevel) {
-                case GenericLogLevel.Trace:
-                    return LogEventLevel.Verbose;
-                case GenericLogLevel.Debug:
-                    return LogEventLevel.Debug;
-                case GenericLogLevel.Info:
-                    return LogEventLevel.Information;
-                case GenericLogLevel.Warn:
-                    return LogEventLevel.Warning;
-                case GenericLogLevel.Error:
-                    return LogEventLevel.Error;
-                case GenericLogLevel.Fatal:
-                    return LogEventLevel.Fatal;                  
-            }
-            return LogEventLevel.Information;
-        }
-
-        private static string AggregatePushLogItems(List<PushLogItem> pushLogItems)
+      try
+      {
+        foreach (var extraProperty in logEvent.ExtraProperties)
         {
-            if (pushLogItems == null || pushLogItems.Count == 0) { return string.Empty; }
-            var sb = new StringBuilder();
-
-            foreach (var item in pushLogItems)
-            {
-                sb.AppendLine(item.ToString());          
-            }
-            return sb.ToString();
+          logger = logger.ForContext(extraProperty.Key, extraProperty.Value);
         }
+      }
+      catch (Exception ex)
+      {
+        Debug.Assert(true, "Serilog Agent Context Generation Threw Exception", ex.Message);
+      }
+
+      logger.Write(GenericToSpecificLogLevel(logEvent.LogLevel), logEvent.Exception, logEvent.Message);
     }
+
+    private static LogEventLevel GenericToSpecificLogLevel(GenericLogLevel logLevel)
+    {
+      switch (logLevel)
+      {
+        case GenericLogLevel.Trace:
+          return LogEventLevel.Verbose;
+        case GenericLogLevel.Debug:
+          return LogEventLevel.Debug;
+        case GenericLogLevel.Info:
+          return LogEventLevel.Information;
+        case GenericLogLevel.Warn:
+          return LogEventLevel.Warning;
+        case GenericLogLevel.Error:
+          return LogEventLevel.Error;
+        case GenericLogLevel.Fatal:
+          return LogEventLevel.Fatal;
+      }
+      return LogEventLevel.Information;
+    }
+
+    private static string AggregatePushLogItems(List<PushLogItem> pushLogItems)
+    {
+      if (pushLogItems == null || pushLogItems.Count == 0) { return string.Empty; }
+      var sb = new StringBuilder();
+
+      foreach (var item in pushLogItems)
+      {
+        sb.AppendLine(item.ToString());
+      }
+      return sb.ToString();
+    }
+  }
 }
