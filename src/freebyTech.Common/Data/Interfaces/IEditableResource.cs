da71021baf2b70a8c40079fdfc1436ee146faef3
James Eby <admin@freebytech.com>
2023-02-06 22:29:34 -0600

    Updated to allow for other ID types besides Guid.

---
+++ src/freebyTech.Common/Data/Interfaces/IEditableResource.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace freebyTech.Common.Data.Interfaces
+{
+  /// <summary>
+  /// Interface represents all the properties that standard editable data times should implement within freebyTech systems.
+  /// </summary>
+  public interface IEditableResource
+  {
+    byte[] Ts { get; set; }
+
+    // This properties are added after the fact by scaffolding and are used as non entity framework specific state information
+    // used by the client UI for change tracking.
+    bool IsNew { get; set; }
+    bool IsDirty { get; set; }
+    bool IsDeleted { get; set; }
+  }
+}
