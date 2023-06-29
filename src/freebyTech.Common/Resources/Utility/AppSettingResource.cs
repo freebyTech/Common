39a99590b7e1646e2abc5138ec3507aafc1307f7
James Eby <admin@freebytech.com>
2023-06-28 23:52:37 -0500

    Fixed copypasta error.

---
+++ src/freebyTech.Common/Resources/Utility/AppSettingResource.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace freebyTech.Common.Resources.Utility;
+
+/// <summary>
+/// The resource used to store runtime application settings in the database.
+/// </summary>
+public partial class AppSettingResource : BaseResource<int>
+{
+  /// <summary>
+  /// Gets or sets the name for this application setting.
+  /// </summary>
+  /// <value>
+  /// The name.
+  /// </value>
+  public string Name { get; set; }
+
+  /// <summary>
+  /// Gets or sets the description for this application setting.
+  /// </summary>
+  /// <value>
+  /// The name.
+  /// </value>
+  public string Description { get; set; }
+
+  /// <summary>
+  /// Gets or sets the value for this application setting.
+  /// </summary>
+  /// <value>
+  /// The name.
+  /// </value>
+  public string Value { get; set; }
+
+  /// <summary>
+  /// Gets or sets the level for which this value applies.
+  /// </summary>
+  /// <value>
+  /// The name.
+  /// </value>
+  public string Level { get; set; }
+}
