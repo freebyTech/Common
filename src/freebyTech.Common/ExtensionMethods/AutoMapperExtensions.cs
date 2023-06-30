1c46afb203f1ce9c04f267db56dd7ae043518809
James Eby <admin@freebytech.com>
2023-06-30 14:50:11 -0500

    Changing mapping methods.

--- src/freebyTech.Common/ExtensionMethods/AutoMapperExtensions.cs
+++ src/freebyTech.Common/ExtensionMethods/AutoMapperExtensions.cs
@@ -6,27 +6,48 @@ namespace freebyTech.Common.ExtensionMethods;
 public static class AutoMapperExtensions
 {
   /// <summary>
-  /// Will create a standard resource to entity mapping.
+  /// Will create a standard resource to entity mapping and vice versa.
   /// </summary>
   /// <returns></returns>
-  public static void CreateResourceToEntityMap<TEntity, TResource>(this Profile profile)
+  public static void CreateEAndRMaps<TEntity, TResource>(this Profile profile)
     where TEntity : new()
     where TResource : new()
   {
-    profile.CreateMap<TEntity, TResource>();
     profile.CreateMap<TResource, TEntity>();
+    profile.CreateMap<TEntity, TResource>();
   }
 
   /// <summary>
   /// Will create a standard resource to entity mapping.
   /// </summary>
   /// <returns></returns>
-  public static void CreateSimpleResourceViewToEntityMap<TEntity, TResource>(this Profile profile)
+  public static IMappingExpression<TResource, TEntity> CreateRToEMap<TResource, TEntity>(this Profile profile)
+    where TEntity : new()
+    where TResource : new()
+  {
+    return profile.CreateMap<TResource, TEntity>();
+  }
+
+  /// <summary>
+  /// Will create a standard resource to entity mapping.
+  /// </summary>
+  /// <returns></returns>
+  public static IMappingExpression<TEntity, TResource> CreateEToRMap<TEntity, TResource>(this Profile profile)
+    where TEntity : new()
+    where TResource : new()
+  {
+    return profile.CreateMap<TEntity, TResource>();
+  }
+
+  /// <summary>
+  /// Will create a simple resource view to entity mapping.
+  /// </summary>
+  /// <returns></returns>
+  public static IMappingExpression<TResource, TEntity> CreateSrvToEMap<TResource, TEntity>(this Profile profile)
     where TEntity : IEditableModel, new()
     where TResource : IEditableResource, new()
   {
-    profile.CreateMap<TEntity, TResource>();
-    profile
+    return profile
       .CreateMap<TResource, TEntity>()
       .ForMember(dest => dest.CreatedOn, opt => opt.Ignore())
       .ForMember(dest => dest.CreatedBy, opt => opt.Ignore())
