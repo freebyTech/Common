4b6ab45151ae1be47d07c0b572dd47332dc8e950
James Eby <admin@freebytech.com>
2023-06-30 13:53:22 -0500

    Fix mapping issues.

--- src/freebyTech.Common/ExtensionMethods/AutoMapperExtensions.cs
+++ src/freebyTech.Common/ExtensionMethods/AutoMapperExtensions.cs
@@ -10,8 +10,8 @@ public static class AutoMapperExtensions
   /// </summary>
   /// <returns></returns>
   public static void CreateResourceToEntityMap<TEntity, TResource>(this Profile profile)
-    where TEntity : IEditableModel, new()
-    where TResource : IEditableModel, new()
+    where TEntity : new()
+    where TResource : new()
   {
     profile.CreateMap<TEntity, TResource>();
     profile.CreateMap<TResource, TEntity>();
@@ -25,12 +25,7 @@ public static class AutoMapperExtensions
     where TEntity : IEditableModel, new()
     where TResource : IEditableResource, new()
   {
-    profile
-      .CreateMap<TEntity, TResource>()
-      .ForMember(dest => dest.CreatedOn, opt => opt.Ignore())
-      .ForMember(dest => dest.CreatedBy, opt => opt.Ignore())
-      .ForMember(dest => dest.ModifiedOn, opt => opt.Ignore())
-      .ForMember(dest => dest.ModifiedBy, opt => opt.Ignore());
+    profile.CreateMap<TEntity, TResource>();
     profile
       .CreateMap<TResource, TEntity>()
       .ForMember(dest => dest.CreatedOn, opt => opt.Ignore())
