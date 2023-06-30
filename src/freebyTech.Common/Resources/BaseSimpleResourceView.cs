590f1f48d752060f698399873c5a6bedf38e1532
James Eby <admin@freebytech.com>
2023-06-30 10:00:42 -0500

    Adding more simplified views to resources that will allow lowering of footprints.

---
+++ src/freebyTech.Common/Resources/BaseSimpleResourceView.cs
@@ -0,0 +1,19 @@
+using System;
+using freebyTech.Common.Data.Interfaces;
+
+namespace freebyTech.Common.Resources;
+
+/// <summary>
+/// The Base Simple Resource View which doesn't contain DB write time and user information.
+/// Simplifying the return of data for less size.
+/// </summary>
+public class BaseSimpleResourceView<IDType> : BaseSimpleResourceViewNonActive<IDType>
+{
+  /// <summary>
+  /// Gets or sets a value indicating whether this instance is active.
+  /// </summary>
+  /// <value>
+  ///   <c>true</c> if this instance is active; otherwise, <c>false</c>.
+  /// </value>
+  public bool IsActive { get; set; }
+}
